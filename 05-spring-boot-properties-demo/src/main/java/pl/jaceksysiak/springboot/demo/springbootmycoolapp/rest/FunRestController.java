ac8b231a58aa48984e37e572f8606d03933ef1c6
Jacek <Jacek@BERLIN>
2020-03-18 07:35:00 +0100

    nmnmn

--- 05-spring-boot-properties-demo/src/main/java/pl/jaceksysiak/springboot/demo/springbootmycoolapp/rest/FunRestController.java
+++ 05-spring-boot-properties-demo/src/main/java/pl/jaceksysiak/springboot/demo/springbootmycoolapp/rest/FunRestController.java
@@ -2,14 +2,25 @@ package pl.jaceksysiak.springboot.demo.springbootmycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@GetMapping("/teaminfo")
+	public String teamInfo() {
+		return "Team info " + coachName + " " + teamName;
+	}
 		
 	// expose "/" that return "Hello World"
-	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World! Time on server is " + LocalDateTime.now();
